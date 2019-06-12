8b75545f6cec6a4364010144bcce24527d853d6c
junbumshin <51726125+junbumshin@users.noreply.github.com>
2019-06-12 15:42:49 +0900

    Update priority_queue.cc
    comment 반영했습니다. 
    ----------------------
    class의 이름은 대문자로 시작하는 것이 가독성에 도움이 될것 같습니다.

--- priority_queue.cc
+++ priority_queue.cc
@@ -1,38 +1,38 @@
 #include "priority_queue.h"
 
 template <>
-templatePriorityQueue<int>::templatePriorityQueue(){
+TemplatePriorityQueue<int>::templatePriorityQueue(){
 }
 
 template <>
-templatePriorityQueue<int>::~templatePriorityQueue(){
+TemplatePriorityQueue<int>::~templatePriorityQueue(){
 }
 
 template <>
-bool templatePriorityQueue<int>::empty() const{
+bool TemplatePriorityQueue<int>::empty() const{
   return storage.empty();
 }
 
 template <>
-int templatePriorityQueue<int>::size() {
+int TemplatePriorityQueue<int>::size() {
   return storage.size();
 }
 
 template <>
-const int& templatePriorityQueue<int>::top() const{
+const int& TemplatePriorityQueue<int>::top() const{
   if (storage.empty())
     return 0;  // TODO(junbumshin) check return 0 is allowed as a return of int& 
   return storage.top();
 }
 
 template <>
-void templatePriorityQueue<int>::pop(){
+void TemplatePriorityQueue<int>::pop(){
   if (storage.empty())
     return 0;  // TODO(junbumshin) check return 0 is allowed as a return of int& 
   storage.pop();
 }
 
 template <>
-void templatePriorityQueue<int>::push(const int& val){
+void TemplatePriorityQueue<int>::push(const int& val){
   storage.push(val);
 }
