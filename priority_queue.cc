db7c42dab99b815368808cc40c1c5de2cf762685
junbumshin <51726125+junbumshin@users.noreply.github.com>
2019-06-12 15:38:01 +0900

    Update priority_queue.cc
    Comment 반영 중입니다.

--- priority_queue.cc
+++ priority_queue.cc
@@ -10,25 +10,29 @@ templatePriorityQueue<int>::~templatePriorityQueue(){
 
 template <>
 bool templatePriorityQueue<int>::empty() const{
-        return storage.empty();
+  return storage.empty();
 }
 
 template <>
 int templatePriorityQueue<int>::size() {
-        return storage.size();
+  return storage.size();
 }
 
 template <>
 const int& templatePriorityQueue<int>::top() const{
-        return storage.top();
+  if (storage.empty())
+    return 0;  // TODO(junbumshin) check return 0 is allowed as a return of int& 
+  return storage.top();
 }
 
 template <>
 void templatePriorityQueue<int>::pop(){
-        storage.pop();
+  if (storage.empty())
+    return 0;  // TODO(junbumshin) check return 0 is allowed as a return of int& 
+  storage.pop();
 }
 
 template <>
 void templatePriorityQueue<int>::push(const int& val){
-        storage.push(val);
+  storage.push(val);
 }
